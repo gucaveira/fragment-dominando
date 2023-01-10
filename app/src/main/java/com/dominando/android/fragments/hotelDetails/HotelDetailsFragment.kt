d96888456f77c72a03386548b7816ad5885aedcc
gustavo pereira da rocha <pereiragustavo215@gmail.com>
2023-01-10 19:00:15 -0300

    compartilhando informação

--- app/src/main/java/com/dominando/android/fragments/hotelDetails/HotelDetailsFragment.kt
+++ app/src/main/java/com/dominando/android/fragments/hotelDetails/HotelDetailsFragment.kt
@@ -1,9 +1,14 @@
 package com.dominando.android.fragments.hotelDetails
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.ShareActionProvider
+import androidx.core.view.MenuItemCompat
 import androidx.fragment.app.Fragment
 import com.dominando.android.fragments.R
 import com.dominando.android.fragments.databinding.FragmentHotelDetailsBinding
@@ -15,10 +20,15 @@ import com.dominando.android.fragments.repository.MemoryRepository
 class HotelDetailsFragment : Fragment(), HotelDetailsView {
     private val presenter = HotelDetailsPresenter(this, MemoryRepository)
     private var hotel: Hotel? = null
+    private var shareActionProvider: ShareActionProvider? = null
 
     private var _binding: FragmentHotelDetailsBinding? = null
     private val binding: FragmentHotelDetailsBinding get() = _binding!!
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,6 +41,28 @@ class HotelDetailsFragment : Fragment(), HotelDetailsView {
         super.onViewCreated(view, savedInstanceState)
         presenter.loadHotelDetails(arguments?.getLong(EXTRA_HOTEL_ID, -1) ?: -1)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.hotel_details, menu)
+        val shareItem = menu.findItem(R.id.action_share)
+        shareActionProvider = MenuItemCompat.getActionProvider(shareItem) as? ShareActionProvider
+        setShareIntent()
+    }
+
+    private fun setShareIntent() {
+        val text = getString(
+            R.string.share_text,
+            hotel?.name,
+            hotel?.rating
+        )
+        shareActionProvider?.setShareIntent(Intent(Intent.ACTION_SEND).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT)
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, text)
+        })
+    }
+
 
     override fun showHotelDetails(hotel: Hotel) {
         this.hotel = hotel
